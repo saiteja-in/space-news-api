6d3d4ce903e312a9984b189387a23edcb43355ff
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-24 13:15:03 +0530

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 const App = () => {
-  useEffect(()=>{
+  const [newsData, setNewsData] = useState([]);
+
+  useEffect(() => {
     fetch("https://api.spaceflightnewsapi.net/v3/articles")
-    .then((res)=>res.json())
-    .then((data)=>console.log(data))
-  })
+      .then((res) => res.json())
+      .then((data) => setNewsData(data));
+  }, []);
 
   return (
     <div className='App'>
-     <div className='title'>
-      <h1>Space News</h1>
-     </div>
-     <div className='newsContainer'></div>
+      <div className='title'>
+        <h1>Space News</h1>
+      </div>
+      <div className='newsContainer'>
+        {newsData.map((val, key) => (
+          <div key={key} className='newsItem'>
+            <div className='imageContainer'>
+              <img src={val.imageUrl} alt={val.title} />
+            </div>
+            <h3>{val.title}</h3>
+            <p>{val.summary}</p>
+            <h4>{val.publishedAt}</h4>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
